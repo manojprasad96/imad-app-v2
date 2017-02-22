07e50e8e586780826cc0f8c933da044b9b6e9e0e
manojprasad96 <v.manojprasad@gmail.com>
2017-02-22 23:21:45 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,7 @@
 console.log('Loaded!');
+var element1=document.getElementById('txt');
+element1.innerHTML='Hai this is client';
+var img=document.getElementById('imag');
+img.onclick= function (){
+    img.style.marginLeft='100px';
+};

8c178891bcdfab102da40f051d22915d5e53353a
manojprasad96 <v.manojprasad@gmail.com>
2017-02-23 21:59:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,15 +2,20 @@ console.log('Loaded!');
 var element1=document.getElementById('txt');
 element1.innerHTML='Hai this is client';
 var img=document.getElementById('imag');
-var margin=0;
+function margin(){
+var margin=0;    
+}
+
+function moveLeft(){
+    margin=margin+10;
+    img.style.marginLeft=margin+'px';
+}
+function moveRight(){
+    margin=margin+10;
+    img.style.marginRight=margin+'px';
+}
 img.onclick= function (){
-    for(i=0;i<5;i++){
-        margin=margin+10;
-        img.style.marginLeft=margin+'px';
-    }
-    margin=0;
-    for(i=0;i<5;i++){
-        margin=margin+10;
-        img.style.marginRight=margin+'px';
-    }
+     var interval1=setInterval(moveLeft,100);
+     margin();
+     var interval2=setInterval(moveRight,50);
 };

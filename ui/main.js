55c14e9cf3b4f787400cc1fd2ae476ad66a3d02e
manojprasad96 <v.manojprasad@gmail.com>
2017-02-23 22:07:57 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,20 +2,15 @@ console.log('Loaded!');
 var element1=document.getElementById('txt');
 element1.innerHTML='Hai this is client';
 var img=document.getElementById('imag');
-function margin(){
+
 var margin=0;    
-}
 
 function moveLeft(){
     margin=margin+1;
     img.style.marginLeft=margin+'px';
 }
-function moveRight(){
-    margin=margin+1;
-    img.style.marginRight=margin+'px';
-}
+
 img.onclick= function (){
-     var interval1=setInterval(moveLeft,100);
-     margin();
-     var interval2=setInterval(moveRight,50);
+     var interval1=setInterval(moveLeft,50);
+     
 };
